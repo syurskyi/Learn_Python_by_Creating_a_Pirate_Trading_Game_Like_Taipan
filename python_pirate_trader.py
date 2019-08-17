243bd868d120a6a1aefea294a25b6f5e70d9c81f
Grey <sergejyurskyj@yahoo.com>
2019-08-16 23:59:44 -0700

    Commit the fist section Done

--- python_pirate_trader.py
+++ python_pirate_trader.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 
 MENU_DIVIDER = "-------------------------------------"
 GAME_TITLE = "Python Pirate Trader 0.1A"
@@ -33,8 +34,14 @@ def get_starting_options():
     return opts
 
 
-def leave_port():
-    pass
+def leave_port(city_list, current_date):
+    i = 1
+    for city in city_list:
+        print("{0} {1}".format(i, city['name']))
+        i = i + 1
+    select_city = input("Which city do you wish to go to?: ")
+    current_date += datetime.timedelta(days=1)
+    return city_list[int(select_city)-1], current_date
 
 
 def buy():
@@ -45,12 +52,22 @@ def sell():
     input("What do you want to sell?")
 
 
+def visit_bank():
+    input("How much to transfer to the bank?: ")
+
 # Start Game
 welcome_message()
 firm_name = get_firm_name()
 cash, debt, cannons = get_starting_options()
 
+cities = ({'name': 'Hong Kong', 'has_warehouse': True, 'has_bank': True},
+          {'name': 'Shangai', 'has_warehouse': False, 'has_bank': False},
+          {'name': 'London', 'has_warehouse': False, 'has_bank': False})
+
+current_city = cities[0]
 game_running = True
+current_date = datetime.datetime(1820, 1, 1)
+
 while game_running:
     # Display Main Game Interface
     os.system("cls")  # clear terminal
@@ -75,18 +92,27 @@ while game_running:
     print(f"Debt: {debt}")
     print("Cannons: %d " % cannons)
 
-    print("City: [current_city]")
-    print("Date:")
+    print("City: %s" % current_city['name'])
+    # print("Date: {}".format(current_date))
+    # http://strtime.org
+    print("Date: {:%B %d, %Y}".format(current_date))
 
     print(MENU_DIVIDER)
 
-    print("Menu: L)eave Port, B)uy, S)ell, T)ransfer Bank, V)isit the Money Lender, Q)uit")
+    has_bank_string = ""
+    if current_city['has_bank'] == True:
+        has_bank_string = "V)isit Bank"
+
+    print("Menu: L)eave Port, B)uy, S)ell, T)ransfer Warehouse, %s Q)uit" % has_bank_string)
     menu_option = input("What is your options?:")
     if menu_option == "L":
-        leave_port()
+        current_city, current_date = leave_port(cities, current_date)
     elif menu_option =="B":
         buy()
     elif menu_option == "S":
         sell()
+    elif menu_option == "V" and current_city['has_bank'] == True:
+        visit_bank()
     elif menu_option == "Q":
         game_running = False
+
